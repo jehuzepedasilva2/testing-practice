f93872686b54c1a867dec74ad2bbea13996bde24
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2024-12-03 12:10:57 -0800

    added two test to test file for capitalize

--- Test.js
+++ Test.js
@@ -1,7 +1,12 @@
 const ftt = require('./FunctionsToTest.js'); // functions to test (object)
 
 const helloString = 'hello';
+const myName = 'jehu';
 
 test('capitalize "hello"', () => {
   expect(fft.capitalize(helloString)).toMatch(/Hello/);
+})
+
+test('capitalize my name', () => {
+  expect(fft.capitalize(myName)).toMatch(/Jehu/);
 })
